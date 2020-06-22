33a0355d46ce7c0c8abf9f67216f077e497d4e79
Eli Landau <eli.b.landau@gmail.com>
2020-06-22 15:02:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,71 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  1 + rand(11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total (num)
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game (num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  num = deal_card + deal_card
+  print display_card_total(num)
+  num
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit? (num)
+  prompt_user
+  input = get_user_input
+  
+  if input == "s"
+    num
+  elsif input == "h"
+    num += deal_card
+  else
+    invalid_command
+    hit? (num)
+  end
+  num
+end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  num = initial_round
+  
+  
+    hit? (num) until num > 21 
+      if get_user_input == "s"
+        hit? (num)
+      elsif get_user_input == "h"
+        display_card_total (num)
+      end
+    end_game (num)
 end
     
